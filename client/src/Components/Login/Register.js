a276a34213325c1572a979a23c36d431152abb46
AtlaSociety <minasworku@gmail.com>
2021-12-24 07:19:55 +0300

    Add register page and css

---
+++ client/src/Components/Login/Register.js
@@ -0,0 +1,13 @@
+
+
+const Login = ()=> {
+  return (
+    <div className="App">
+      <form method = "post" action="">
+      <legend forName="name">Name:</legend><input type="name" name="name"/>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
