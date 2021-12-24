b08c8ecf995b975d707b76f37a0bb58cfc2876a4
AtlaSociety <minasworku@gmail.com>
2021-12-24 21:32:26 +0300

    Crafting signup page

--- api/App.js
+++ api/App.js
@@ -1,3 +1,12 @@
-const React = require('react');
+const mongoose = require('mongoose');
+
+const express = require('express');
+
+const app = express();
+
+app.listen("3030",()=>{
+	console.log("server listening on port 3030");
+});
 
 
+module.exports = app;
