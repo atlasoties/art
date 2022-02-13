9d0c8401e91b9b86bbcf71a410a4367f56ce7f70
AtlaSociety <minasworku@gmail.com>
2022-02-13 17:25:30 +0300

    separating the api and client folder

--- api/App.js
+++ api/App.js
@@ -1,14 +1,27 @@
 const mongoose = require('mongoose');
-
+const path = require('path');
 const express = require('express');
 const PORT = 3030;
 const app = express();
 
-app.listen(PORT,()=>{
-	console.log("server listening on port 3030");
-});
+
 
 app.get('/one',(req,res,next) => {
-	res.status(200).send("Hellllo");
+	res.sendFile(path.join(__dirname,"./data.html"));
+});
+app.get('/one',(req,res,next) => {
+	res.end("World");
+});
+
+
+app.get('/two',(req,res,next) => {
+	res.write("Hellllo");
+	next();
+},(req,res,next) => {
+	res.end("World");
+});
+
+app.listen(PORT,()=>{
+	console.log("server listening on port 3030");
 });
 
