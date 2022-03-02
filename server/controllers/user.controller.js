c65eccdb2eb029bc3a24e0eecffe9a1fbc5146fd
AtlaSociety <minasworku@gmail.com>
2022-03-02 17:32:40 +0300

    user api bug fixed and update

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -5,30 +5,30 @@ const User = mongoose.model("User");
 class UserActions{
 	
 
-async create = (request ,response) => {
+ async create (request ,response) {
 
 	const {name , email , password, dob} = request.body;
 	
 	
 		if(name && email && password && dob){
-			let exception=" ";
+			
 			const regExp = /@gmail.com|@yahoo.com|@facebook.com|@live.com/;
 			if(!regExp.test(email)) response.status(404).json("Email domain is not supported");
 	
-			if(password.length < 8) response.status(404).json("Password length must be equal to 8 characters or above");
-			
-				const user = await new User(
-					{
-					name,
-					email,
-					password:hash(password+process.env.SALT),
-					dob
-				});
-				user.save();
-				response.json({
-					message:"user registerd successfully"
-				});	
-	
+			else if(password.length < 8) response.status(404).json("Password length must be equal to 8 characters or above");
+			else{
+					const user = await new User(
+						{
+						name,
+						email,
+						password:hash(password+process.env.SALT),
+						dob
+					});
+					user.save();
+					response.json({
+						message:"user registerd successfully"
+					});	
+			}
 		}
 		else{
 			response.status(400).json("All fields should be filled in")
@@ -49,21 +49,28 @@ async create = (request ,response) => {
 			}else{
 				response.status(404).json("Your Credential does not match");
 			}
-		}else{
+		}
+		else{
 			response.status(400).json("All fields should be filled in");
 		}
 	}
 	
-	update (request, response){
-		
+	async update (request, response){
+			const id = request.params.id;
+
 	}
 	
-	 deletes(request, response){
-		
+	async deletes(request, response){
+		const id = request.params.id;
+		await User.findById(id)
+			.then(item => item.remove().then(()=>response.status(200).json("Item Deleted Successfully")))
+			.catch(err=> response.status(404).json("The User doesn't Exist in the database"));
 	}
 	
-	show (request, response){
-		
+	async show (request, response){
+		await User.find()
+				  .sort({date:-1})
+				  .then(items => response.json(items));
 	}
 }
 
