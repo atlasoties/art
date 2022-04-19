ce99bb5d40beacfdb8eff22188bc84cad2f5d76a
AtlaSociety <minasworku@gmail.com>
2022-04-19 20:50:17 +0300

    Some unnecessary changes

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8000;
 const app = express();
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
