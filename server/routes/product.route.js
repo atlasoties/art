0b285dc55a04ce877b3a13e1b12642062ed27e55
AtlaSociety <minasworku@gmail.com>
2022-05-06 23:25:02 +0300

    protected middleware added on products and users route

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const { add, update, show } = require('../controllers/product.controller')
+const {protect} = require("../middleware/auth.middleware");
 
-router.post('/add', add);
+router.post('/add',protect, add);
 // router.delete('/delete/:id', delete);
-router.put('/update/:id', update);
-router.get('/show', show);
+router.put('/update/:id',protect, update);
+router.get('/show', protect,show);
 
 module.exports = router;
